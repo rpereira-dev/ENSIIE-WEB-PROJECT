86cdd257e3a60b5bce85762b666b4116c7f14a4b
rpereira <romain.pereira@ensiie.fr>
2018-05-16 18:24:22 +0200

    rapport

--- public/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require('../vendor/autoload.php');
-
-use Model\ULC\Discord\API;
-
-$client = API::client();
-$client->channel->createMessage(['channel.id' => 440662985997025280, 'content' => 'Coucou <@382175522832121857>, ça roule']);
